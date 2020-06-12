93552d6a96314f0f0cee7031aa93f50ed119e924
Scott Mittman <sm2884@nau.edu>
2020-06-12 01:48:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
   for (let i=0;i<cart.length-1;i++){
     str+=`${cart[i].itemName} at \$${cart[i].itemPrice}, `
   }
-  str+=`and ${cart[length-1].itemName} at \$${cart[length-1].itemPrice}.`
+  str+=`and ${cart[cart.length-1].itemName} at \$${cart[cart.length-1].itemPrice}.`
   return str
 }
 
