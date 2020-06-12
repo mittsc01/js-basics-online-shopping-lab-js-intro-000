90ccac5275dbe1e06745db48b2e10435ebdb3690
Scott Mittman <sm2884@nau.edu>
2020-06-12 02:12:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (arguments.length===0){
+  if (args.length===0){
     //console.log("hi")
     console.log("Sorry, we don't have a credit card file for you.")
   }
